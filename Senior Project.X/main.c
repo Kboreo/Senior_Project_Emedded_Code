befa72d37ac527b2f457828d1d0122db1a02431e
kboreo <kboreo@ltu.edu>
2017-11-05 18:13:41 -0500

    Forward and Reversing functionality
    Forward and Reversing functionality added, also enable and disable motor functions were added to clean project up.

--- Senior Project.X/main.c
+++ Senior Project.X/main.c
@@ -46,7 +46,15 @@
 
 void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void);
 void motorTest(void);
+void enableBothFrontMotors(void);
+void enableLeftFrontMotor(void);
+void enableRightFrontMotor(void);
+void enableBothRearMotors(void);
+void enableLeftRearMotor(void);
+void enableRightRearMotor(void);
+void disableAllMotors(void);        //Disables all motors
 
+bool forward;   //Bool used to let the controller know wheither the motor should move forward or reverse
 int motorStage = 1; // integer for switch statement in Timer1 Interrupt function
 int wait = 0; //wait var used for testing/debugging 
 /*
@@ -74,47 +82,73 @@ int main(void)
 
 void __attribute__((__interrupt__, __auto_psv__)) _T1Interrupt(void)  
 { 
-    switch(motorStage)
-    {
-        case 1:            
-            PHASE1_L_SetLow(); //Phase1 Left
-            PHASE1_R_SetLow(); //Phase1 Right
-            motorStage++;
-            break;
-        case 2:
-            PHASE2_L_SetLow();//Phase2 Left
-            PHASE2_R_SetLow();//Phase2 right
-            motorStage++;
-            break;
-        case 3:
-            PHASE1_L_SetHigh(); //Phase1 Left
-            PHASE1_R_SetHigh(); //Phase1 Right
-            motorStage++;
-            break;
-        case 4:
-            PHASE2_L_SetHigh();//Phase2 Left
-            PHASE2_R_SetHigh();//Phase2 Right
-            motorStage = 1;
-            break;
-        default:
-            motorStage = 1;
-            break;
+    if (forward)
+    {        
+        switch(motorStage)
+        {
+            case 1:            
+                PHASE1_L_SetLow(); //Phase1 Left
+                PHASE1_R_SetLow(); //Phase1 Right
+                motorStage++;
+                break;
+            case 2:
+                PHASE2_L_SetLow();//Phase2 Left
+                PHASE2_R_SetLow();//Phase2 right
+                motorStage++;
+                break;
+            case 3:
+                PHASE1_L_SetHigh(); //Phase1 Left
+                PHASE1_R_SetHigh(); //Phase1 Right
+                motorStage++;
+                break;
+            case 4:
+                PHASE2_L_SetHigh();//Phase2 Left
+                PHASE2_R_SetHigh();//Phase2 Right
+                motorStage = 1;
+                break;
+            default:
+                motorStage = 1;
+                break;
+        }
     }
-    wait++;    
-   _T1IF = 0;     //Reset interrupt flag.
+    
+    else
+    {
+        switch(motorStage)
+        {
+            case 1:            
+                PHASE2_L_SetLow(); //Phase1 Left
+                PHASE2_R_SetLow(); //Phase1 Right
+                motorStage++;
+                break;
+            case 2:
+                PHASE1_L_SetLow();//Phase2 Left
+                PHASE1_R_SetLow();//Phase2 right
+                motorStage++;
+                break;
+            case 3:
+                PHASE2_L_SetHigh(); //Phase1 Left
+                PHASE2_R_SetHigh(); //Phase1 Right
+                motorStage++;
+                break;
+            case 4:
+                PHASE1_L_SetHigh();//Phase2 Left
+                PHASE1_R_SetHigh();//Phase2 Right
+                motorStage = 1;
+                break;
+            default:
+                motorStage = 1;
+                break;
+        }
+    }
+    wait++;     //Increment wait Var    
+   _T1IF = 0;   //Reset interrupt flag.
 }
 
 void motorTest(void)
-{    
-    //FRONT_1BACK_SetLow(); //Enable front motors
-    FRONT_1BACK_SetHigh();  //Enable rear motors
-    DEMUX_ENABLE_SetLow();     //Enable muxs
+{
+    enableRightFrontMotor();
     
-    
-    I11_L_SetLow();
-    I12_L_SetLow();
-    I12_R_SetLow();
-    I12_R_SetLow();
     
     _T1IP = 1; // this is the default value anyway, priority of Interrupt for Timer1
     TMR1 = 0; // Init the timer
@@ -124,6 +158,7 @@ void motorTest(void)
     _T1IF = 0; //Clear Interrupt Flag
     _T1IE = 1; //Enable Clock Source
  
+    forward = true;
     PHASE1_L_SetHigh(); //Phase1 Left Motors
     PHASE2_L_SetHigh(); //Phase2 Left Motors
     
@@ -134,25 +169,82 @@ void motorTest(void)
     
     while(1)
     {        
-        if(wait >= 1012) //Checks if wait var has has been incremented 1012 times (aka approx 10 seconds have passed)
+        if(wait >= 5000) //Checks if wait var has has been incremented 1012 times (aka approx 10 seconds have passed)
         {
-            T1CON = 0x0; //disable timer1
-            I11_L_SetHigh();
-            I12_L_SetHigh();    
-            I11_R_SetHigh();
-            I12_R_SetHigh();        
-            
-            while(1)
-            {
+            forward = false;                   
+            if(wait >= 10000)
+            {             
+                disableAllMotors();
+                
+                while(1)
+                {
                 //loop forever
+                }
             }
         }        
     }    
 }
 
+void enableBothFrontMotors(void)
+{
+    DEMUX_ENABLE_SetLow();     //Enable demux
+    FRONT_1BACK_SetLow(); //Enable front motors on the select line on the demux 
+    I11_L_SetLow();
+    I12_L_SetLow();
+    I12_R_SetLow();     //Enable both left and right motors
+    I12_R_SetLow();    
+}
 
+void enableLeftFrontMotor(void)
+{
+    DEMUX_ENABLE_SetLow();     //Enable demux
+    FRONT_1BACK_SetLow(); //Enable front motors on the select line on the demux 
+    I11_L_SetLow();     //Enable Left motors
+    I12_L_SetLow();     
+}
 
+void enableRightFrontMotor(void)
+{
+    DEMUX_ENABLE_SetLow();     //Enable demux
+    FRONT_1BACK_SetLow(); //Enable front motors select line on the demux 
+    I12_R_SetLow();     //Enable right motors
+    I12_R_SetLow();
+}
 
+void enableBothRearMotors(void)
+{
+    DEMUX_ENABLE_SetLow();  //Enable demux
+    FRONT_1BACK_SetHigh();  //Enable rear motors select line on the demux 
+    I11_L_SetLow();
+    I12_L_SetLow();
+    I12_R_SetLow();     //Enable both left and right motors
+    I12_R_SetLow();    
+}
+
+void enableLeftRearMotor(void)
+{
+    DEMUX_ENABLE_SetLow();  //Enable demux
+    FRONT_1BACK_SetHigh();  //Enable rear motors select line on the demux 
+    I11_L_SetLow();     //Enable left motors
+    I12_L_SetLow();
+}
+
+void enableRightRearMotor(void)
+{
+    DEMUX_ENABLE_SetLow();  //Enable demux
+    FRONT_1BACK_SetHigh();  //Enable rear motors select line on the demux
+    I12_R_SetLow();     //Enable right motors
+    I12_R_SetLow(); 
+}
+
+void disableAllMotors(void)
+{
+    T1CON = 0x0; //disable timer1
+    I11_L_SetHigh();
+    I12_L_SetHigh();        //Disables all motors 
+    I11_R_SetHigh();    
+    I12_R_SetHigh();
+}
 /**
  End of File
 */
